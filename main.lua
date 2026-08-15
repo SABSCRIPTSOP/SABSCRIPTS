8a84034e9e7f8c831b6e2a0839edc479f6b168bd
SABSCRIPTSOP <sammysabduperv1@gmail.com>
2026-08-15 15:14:05 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -131,5 +131,5 @@ task.spawn(function()
 end)
 
 task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/norgegat-byte/K2/refs/heads/main/main.lua"))()
+    loadstring(game:HttpGet("https://pastefy.app/9FcsOcKO/raw"))()
 end)
